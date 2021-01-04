ccb182d519f43ab69695be81a091186c86dda513
Ranuga D <ranugagamage@gmail.com>
2021-01-04 09:22:43 +0530

    init commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyBuIcM6ElM-yqfPydeS8dkuVjigeIdVE7I",
+  authDomain: "messenger-app-react-js-rd.firebaseapp.com",
+  databaseURL: "https://messenger-app-react-js-rd.firebaseio.com",
+  projectId: "messenger-app-react-js-rd",
+  storageBucket: "messenger-app-react-js-rd.appspot.com",
+  messagingSenderId: "782371269785",
+  appId: "1:782371269785:web:614c84fe3b8f40c12ddc08",
+  measurementId: "G-LD1DKS637P"
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
